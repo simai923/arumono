2cf26aae19fe2ef88dc9cb8cecd626f56928a88d
simai923 <ishun19497@gmail.com>
2021-01-06 20:46:33 +0900

    一覧画面作成

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,5 @@
 class FoodsController < ApplicationController
+  def index
+    @foods = Food.order('foods.limit IS NULL ASC', 'foods.limit ASC')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "introductions#index"
   resources :introductions, only: [:index]
+  resources :foods, only: [:index]
 end
